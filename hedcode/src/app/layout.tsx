9bcfb1ab870bc328788f2f32305de830c0f5db49
Saksit Sirisakda <Saksit.Sirisakda@cr3.group>
2025-05-10 11:30:50 +0700

    setup tailwind and prettier

--- hedcode/src/app/layout.tsx
+++ hedcode/src/app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "HedCode",
-  description: "HedCode is an awesome development team",
+  title: 'HedCode',
+  description: 'HedCode is an awesome development team',
 };
 
 export default function RootLayout({
